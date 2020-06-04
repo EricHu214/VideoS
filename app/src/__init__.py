1bbfb7262c02b4437d4b160875a95d072b1a9860
EricHu214 <Eric.Hu214@gmail.com>
2020-06-04 14:56:06 -0400

    some front end improvements

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -15,7 +15,9 @@ def home():
 def uploadVideo():
     redirect('/')
     if request.method == "POST":
-        if request.files and request.files['video']:
+        option = request.form.get('options')
+
+        if option == '1' and request.files and request.files['video']:
             video = request.files['video']
 
             if video.content_type == "video/mp4":
@@ -29,7 +31,7 @@ def uploadVideo():
                     as_attachment=True,
                     mimetype='video/mp4')
 
-        elif request.form.get('video-url'):
+        elif option == '2' and request.form.get('video-url'):
             cropPercentage = request.form.get('crop_percentage')
             stabilizedVideo = stabilizeYoutube(request.form.get('video-url'), cropPercentage)
 
